c54f8bac9b35e6a4be8973cf09669633c2bf8fcc
Pop <hddev@pop-os.localdomain>
2023-11-13 21:59:44 +0000

    Added VSCode to .gitignore, started writing keyboard component@

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { KeyboardComponent } from '../shared/keyboard/keyboard.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, KeyboardComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'CodeCraft';
+}
---
+++ src/shared/keyboard/keyboard.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-keyboard',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './keyboard.component.html',
+  styleUrl: './keyboard.component.scss'
+})
+export class KeyboardComponent {
+  @Input() numberOfOctaves: number = 1;
+
+  get keys(): string[] {
+    // Generate an array of keys based on the number of octaves
+    const allKeys = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+    const keys: string[] = [];
+
+    for (let i = 0; i < this.numberOfOctaves; i++) {
+      allKeys.forEach(key => {
+        keys.push(key);
+      });
+    }
+
+    return keys;
+  }
+
+  constructor() { }
+}
