6021e1c34e781ef206b8939596d7e14528760e88
Pop <hddev@pop-os.localdomain>
2023-11-16 18:13:31 +0000

    Started chord diagram component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,19 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { KeyboardComponent } from '../shared/keyboard/keyboard.component';
+import { ChordDiagramComponent } from '../shared/chord-diagram/chord-diagram.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, KeyboardComponent,],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    KeyboardComponent, 
+    ChordDiagramComponent, 
+    ChordDiagramComponent
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
